8b82bfe920d2e7071336d625b3386cdc945e9214
Damien_OLSPS <Damien@OLSPS.com>
2018-10-25 16:10:59 +0200

    update

---
+++ Datacamp/sqlalchemy_dataframes.py
@@ -0,0 +1,27 @@
+# import pandas
+import pandas as pd
+
+# Create a DataFrame from the results: df
+df = pd.DataFrame(results)
+
+# Set column names
+df.columns = results[0].keys()
+
+# Print the Dataframe
+print(df)
+
+# Import Pyplot as plt from matplotlib
+import matplotlib.pyplot as plt
+
+# Create a DataFrame from the results: df
+df = pd.DataFrame(results)
+
+# Set Column names
+df.columns = results[0].keys()
+
+# Print the DataFrame
+print(df)
+
+# Plot the DataFrame
+df.plot(kind='bar')
+plt.show()
